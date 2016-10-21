036cc7892538eed34230b1affeb123157aa216dc
devonhackley <devonhackley@gmail.com>
2016-10-21 14:21:01 -0700

    Fifth question completed

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ function sumArray(testArray){ //eslint-disable-line
   var firstArraySum = sum(testArray[0],testArray[1])[0];
   var totalArraySum = sum(firstArraySum,testArray[2])[0];
   var sumArrayTotal = [totalArraySum, testArray[0] + ',' + testArray[1] + ',' + testArray[2] + ' was passed in as an array of numbers, and ' + totalArraySum + ' is their sum.'];
-  // return sumArrayTotal;
+  return sumArrayTotal;
 }
 
 // Here is the test for sumArray(); uncomment it to run it
@@ -104,14 +104,14 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-// function multiplyArray(testArray){ //eslint-disable-line
-//   var firstArrayProd = multiply(testArray[0],testArray[1])[0];
-//   var totalArrayProd = multiply(firstArrayProd,testArray[2])[0];
-//   var prodArrayTotal = [totalArrayProd, testArray[0] + ',' + testArray[1] + ',' + testArray[2] + ' was passed in as an array of numbers, and ' + totalArraySum + ' is their sum.'];
-//   return prodArrayTotal;
-// }
+function multiplyArray(testArray){ //eslint-disable-line
+  var firstArrayProd = multiply(testArray[0],testArray[1])[0];
+  var totalArrayProd = multiply(firstArrayProd,testArray[2])[0];
+  var prodArrayTotal = [totalArrayProd, 'The numbers ' + testArray[0] + ',' + testArray[1] + ',' + testArray[2] + ' have a product of ' + totalArrayProd + '.'];
+  return prodArrayTotal;
+}
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(2,3,4);
+testMultiplyArray(2,3,4);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
